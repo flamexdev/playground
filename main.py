aeed92a4f87d8cb3a4d0cf332fd722d0af020c1d
Flamex <acc@flamex.dev>
2021-11-02 22:28:00 +0100

    fixed bugs

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-class colors:
-  HEADER = '\033[95m'
-  ENDC = '\033[0m'
-
-age = input(colors.HEADER + "Age calculator\n" + colors.ENDC + "What's your age in years? ")
-
-if int(age) > 110:
-  print("How?")
-else:
-  print("you are " + age + " years old.")
+age = input("Age calculator \n\n What's your age in years? ")
+print("You are " + age + " years old.")
