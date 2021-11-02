0a7a82b88bfcffaf386221e6f5753183501d096f
Flamex <acc@flamex.dev>
2021-11-02 22:18:42 +0100

    fixed lunis bug

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ class colors:
   HEADER = '\033[95m'
   ENDC = '\033[0m'
 
-
 age = input(colors.HEADER + "Age calculator\n" + colors.ENDC + "What's your age in years? ")
-print("you are " + age + " years old.")
+
+if int(age) > 110:
+  print("How?")
+else:
+  print("you are " + age + " years old.")
